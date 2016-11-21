fcf0e05b9ed996f75dd27a6fe9decafb57be807f
Friedrich Lindenberg <friedrich@pudo.org>
2016-11-21 09:04:46 +0100

    up the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='countrynames',
-    version='1.0.0',
+    version='1.1.0',
     description="A library to map country names to ISO codes.",
     long_description="",
     classifiers=[
