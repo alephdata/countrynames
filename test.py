82f16f1ea983a241b80356728f75f8bff28254f3
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-07-31 09:50:04 +0200

    Adjust `test.py` to non-fuzzy default

--- test.py
+++ test.py
@@ -14,19 +14,19 @@ tests = [
 print("No fuzzy matching:")
 for test in tests:
     print([test,
-           countrynames.to_code(test, fuzzy=False),
-           countrynames.to_alpha_3(test, fuzzy=False),
-           countrynames.to_name(test, fuzzy=False),
-           countrynames.to_official_name(test, fuzzy=False),
-           countrynames.to_numeric(test, fuzzy=False)
-        ])
-
-print("With fuzzy matching:")
-for test in tests:
-    print([test,
            countrynames.to_code(test),
            countrynames.to_alpha_3(test),
            countrynames.to_name(test),
            countrynames.to_official_name(test),
            countrynames.to_numeric(test)
           ])
+
+print("With fuzzy matching:")
+for test in tests:
+    print([test,
+           countrynames.to_code(test, fuzzy=True),
+           countrynames.to_alpha_3(test, fuzzy=True),
+           countrynames.to_name(test, fuzzy=True),
+           countrynames.to_official_name(test, fuzzy=True),
+           countrynames.to_numeric(test, fuzzy=True)
+        ])
