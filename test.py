e58c43288f05445447b3828f6045442ff564078c
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-08-07 16:47:47 +0200

    Use py.test for testing

--- test.py
+++ test.py
@@ -1,34 +1,33 @@
 # coding: utf-8
-import countrynames
+from countrynames import to_code, to_alpha_3, to_numeric
 
-tests = [
-    'Germany',
-    'DE',
-    'UK',
-    u'Российская Федерация',
-    'Rossiyskaya Federatsiya',
-    'Tgermany',
-    'European Union',
-    'Kosovo',
-    None
-]
 
-print("No fuzzy matching:")
-for test in tests:
-    print([test,
-           countrynames.to_code(test),
-           countrynames.to_alpha_3(test),
-           countrynames.to_name(test),
-           countrynames.to_official_name(test),
-           countrynames.to_numeric(test)
-          ])
+def test_to_code():
+    assert to_code("Germany") == "DE"
+    assert to_code("UK") == "GB"
 
-print("With fuzzy matching:")
-for test in tests:
-    print([test,
-           countrynames.to_code(test, fuzzy=True),
-           countrynames.to_alpha_3(test, fuzzy=True),
-           countrynames.to_name(test, fuzzy=True),
-           countrynames.to_official_name(test, fuzzy=True),
-           countrynames.to_numeric(test, fuzzy=True)
-        ])
+
+def test_to_alpha_3():
+    assert to_alpha_3("Germany") == "DEU"
+    assert to_alpha_3("UK") == "GBR"
+
+
+def test_to_numeric():
+    assert to_numeric("Germany") == "276"
+
+
+def test_unicode():
+    assert to_code(u'Российская Федерация') == "RU"
+
+
+def test_fuzzy_matching():
+    assert to_code('Rossiyskaya Federatsiya', fuzzy=True) == "RU"
+    assert to_code("Falklands Islands", fuzzy=True) == "FK"
+    assert to_code("TGermany", fuzzy=True) == "DE"
+
+
+def test_non_standard_codes():
+    assert to_code("European Union") == "EU"
+    assert to_alpha_3("European Union") == "EUU"
+    assert to_code("Kosovo") == "XK"
+    assert to_alpha_3("Kosovo") == "XKX"
