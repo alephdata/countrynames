3e4571e33120bb4a465eb7e0bc8efb80996b8e95
Friedrich Lindenberg <friedrich@pudo.org>
2021-02-25 14:17:27 +0100

    introduce CI

--- gen.py
+++ gen.py
@@ -5,8 +5,8 @@ import pycountry
 from collections import defaultdict
 
 data = defaultdict(list)
-data_file = os.path.join(os.path.dirname(__file__), 'countrynames', 'data.yaml')
-with open(data_file, 'r') as fh:
+data_file = os.path.join(os.path.dirname(__file__), "countrynames", "data.yaml")
+with open(data_file, "r") as fh:
     for code, names in yaml.load(fh).items():
         data[code].extend(names)
 
@@ -14,7 +14,7 @@ for country in pycountry.countries:
     cc = country.alpha2
     data[cc].append(country.name)
     data[cc].append(country.alpha3)
-    if hasattr(country, 'official_name'):
+    if hasattr(country, "official_name"):
         data[cc].append(country.official_name)
     for loc in babel.localedata.locale_identifiers():
         name = babel.Locale(loc).territories.get(cc)
@@ -23,6 +23,6 @@ for country in pycountry.countries:
 
     data[cc] = list(set(data[cc]))
 
-data_file = os.path.join(os.path.dirname(__file__), 'countrynames', 'data.yaml')
-with open(data_file, 'w') as fh:
+data_file = os.path.join(os.path.dirname(__file__), "countrynames", "data.yaml")
+with open(data_file, "w") as fh:
     yaml.safe_dump(dict(data), fh, default_flow_style=False, allow_unicode=True)
