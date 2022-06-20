d5e2988f3b5306fcaaafdbe04a4562074bcdf6e7
Tarashish Mishra <sunu0000@gmail.com>
2022-06-20 14:35:26 +0530

    Bump version: 1.12.1 → 1.13.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(path, "README.md"), "r") as f:
 
 setup(
     name="countrynames",
-    version="1.12.1",
+    version="1.13.0",
     description="A library to map country names to ISO codes.",
     long_description=readme,
     long_description_content_type="text/markdown",
